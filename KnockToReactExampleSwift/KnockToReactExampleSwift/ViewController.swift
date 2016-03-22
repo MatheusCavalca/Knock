add5daa84d9400e212c492e131e5cc826871725d
Lucas Farah <lucas.farah@me.com>
2016-03-22 18:27:43 -0300

    CHanged name of Swift Example Project

---
+++ KnockToReactExampleSwift/KnockToReactExampleSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KnockToReactExampleSwift
+//
+//  Created by Lucas Farah on 3/22/16.
+//  Copyright © 2016 Lucas Farah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
